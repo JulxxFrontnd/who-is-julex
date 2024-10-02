cbffc5babe36a7ff818c2ffbf6adb1f97131fb9a
JulxxFrontnd <167488328+JulxxFrontnd@users.noreply.github.com>
2024-10-02 11:35:08 +0800

    Routing adjustments

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Homepage from "./Components/Homepage/Homepage";
-import Experience from "./Components/Experience/Experience";
 import Projects from "./Components/Projects/Projects";
-import Contact from "./Components//Contact/Contact";
+import Contact from "./Components/Contact/Contact"; // Fixed the extra slash here
 
 function App() {
   return (
-    <>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/experience" element={<Exp />}></Route>
         <Route path="/projects" element={<Pro />}></Route>
         <Route path="/contact" element={<Cont />}></Route>
 
@@ -31,14 +29,12 @@ function App() {
           }
         ></Route>
       </Routes>
-    </>
+    </Router>
   );
 }
+
 function Home() {
   return <Homepage />;
-}
-function Exp() {
-  return <Experience />;
 }
 function Pro() {
   return <Projects />;
